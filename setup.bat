5a53968ef12bea327ea9b6d4ea5db1ed7bd7e054
Odintsov Micheel <story71.public@gmail.com>
2023-09-14 04:06:22 +0300

    minor fixesx2

--- setup.bat
+++ setup.bat
@@ -4,7 +4,7 @@ if "%VIRTUAL_ENV%"=="" (
     exit /b 1
 )
 
-set CUSTOM_PACKAGES=PyQt5 requests 
+set CUSTOM_PACKAGES=certifi==2023.7.22 charset-normalizer==3.2.0 idna==3.4 pip==23.2.1 PyQt5==5.15.9 PyQt5-Qt5==5.15.2 PyQt5-sip==12.12.2 requests==2.31.0 setuptools==65.5.0 urllib3==2.0.4
 
 python.exe -m pip install --upgrade pip
 
